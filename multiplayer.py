7c751ff5149364effaceea2b8de5aa19e8036f04
Andrew <aschau@uci.edu>
2016-05-02 05:57:31 -0700

    i think i fixed it?

--- multiplayer.py
+++ multiplayer.py
@@ -193,7 +193,8 @@ class multiGame:
     def update(self):
         try:
             if self.serverReady:
-                self.socket.send("RECEIVE:"+self.hostName)
+                if self.clientPlayerNum == 0:
+                    self.socket.send("RECEIVE:"+self.hostName)
             else:
                 self.socket.send("GETGAMESTART:" + self.hostName)
 
@@ -208,8 +209,8 @@ class multiGame:
                     self.playerList[int(modifiedMessage[1])].posx = int(modifiedMessage[2])
                     self.playerList[int(modifiedMessage[1])].posy = int(modifiedMessage[3])
             if modifiedMessage[0] == "SHOOT":
-                if int(modifiedMessage[1]) != self.clientPlayerNum:
-                    self.missiles.append(Missile(int(modifiedMessage[4]), self.playerList[int(modifiedMessage[4])].missileImage, (self.playerList[int(modifiedMessage[4]).posx + (self.playerList[int(modifiedMessage[4])].imagew - 18), self.playerList[int(modifiedMessage[4]).posy - (self.playerList[int(modifiedMessage[4])].imageh)), 8, 32))
+                if int(modifiedMessage[3]) != self.clientPlayerNum:
+                    self.missiles.append(Missile(int(modifiedMessage[3]), self.playerList[int(modifiedMessage[3])].missileImage, (int(modifiedMessage[1]) + self.playerList[int(modifiedMessage[3])].imagew - 18, int(modifiedMessage[2]) - self.playerList[int(modifiedMessage[3])].imageh),8, 32)) #(self.playerList[int(modifiedMessage[3])].posx + (self.playerList[int(modifiedMessage[3])].imagew - 18), self.playerList[int(modifiedMessage[3])].posy - (self.playerList[int(modifiedMessage[3])].imageh)), 8, 32))
         except:
             pass
         if self.serverReady:
@@ -320,6 +321,7 @@ class multiGame:
 
                 if keys[pygame.K_SPACE]:
                     if self.playerList[self.clientPlayerNum].missileCount < self.playerList[self.clientPlayerNum].missileCap:
+                        self.socket.send("SHOOT:" + self.hostName + ":" + str(self.playerList[self.clientPlayerNum].posx) + ":" + str(self.playerList[self.clientPlayerNum].posy) + ":" + str(self.clientPlayerNum))
                         self.missiles.append(self.playerList[self.clientPlayerNum].fire())
                         self.soundManager.playSound("Player_Shoot.ogg")
 
@@ -373,7 +375,6 @@ class multiGame:
             if attacker == self.clientPlayerNum:
                 if ((self.missiles[numMissiles].posy + self.missiles[numMissiles].imageh) <= 0):
                     #self.socket.send("SHOOT:" + self.hostName + ":" + str(self.clientPlayerNum))
-                    self.socket.send("SHOOT:" + self.hostName + ":" + str(self.playerList[self.clientPlayerNum].posx) + ":" + str(self.playerList[self.clientPlayerNum].posy) + ":" + str(self.clientPlayerNum))
                     self.missiles.pop(numMissiles)
                     self.playerList[self.clientPlayerNum].missileCount -= 1
 
